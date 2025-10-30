07c22a55ac1948ac1a873da593dba28ff17d6209
mfagial2016 <m.f.agial2016@gmail.com>
2025-10-30 10:57:17 +0300

    Update and rename server.js to index.js

---
+++ index.js
@@ -0,0 +1,101 @@
+[
+    {
+        "key": "dbln",
+        "value": "%7B%2261575315777415%22%3A%22lfUSzYAu%22%7D",
+        "domain": "facebook.com",
+        "path": "/login/device-based/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "datr",
+        "value": "vjsraBlKO-RajE6EGPz_4wuN",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "sb",
+        "value": "qS5iaKGx_z1zcQ7Ko8r6TbTR",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "ps_l",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "ps_n",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "c_user",
+        "value": "61575315777415",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "ar_debug",
+        "value": "1",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "wd",
+        "value": "1024x607",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "fr",
+        "value": "1GDxmVYUt4sRzs2Xs.AWdBE1d52BjhlQ0QrmJmfemqeVWXdj3sd8fUaickby6zmoeZtHM.BpAwva..AAA.0.0.BpAwva.AWcluomvZOkuJjPBARUMyNOYtns",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "xs",
+        "value": "4%3AcvmXC1hblT7P6Q%3A2%3A1760815409%3A-1%3A-1%3A%3AAcyFzaz4t-_qUX2yE-kcNalyU2M75QOmM_lxa5ov7w",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    },
+    {
+        "key": "presence",
+        "value": "C%7B%22lm3%22%3A%22g.4299025330363844%22%2C%22t3%22%3A%5B%7B%22o%22%3A0%2C%22i%22%3A%22g.813395258117901%22%7D%5D%2C%22utc3%22%3A1761807419720%2C%22v%22%3A1%7D",
+        "domain": "facebook.com",
+        "path": "/",
+        "hostOnly": false,
+        "creation": "2025-10-30T06:59:32.770Z",
+        "lastAccessed": "2025-10-30T06:59:32.770Z"
+    }
+]
